60c67e20690e95583284d503d1bf5612f58d96d7
arora8055 <aroraaman0101@gmail.com>
2020-09-07 22:10:49 +0530

    Uncommented Code

--- test_case_criminal_data.py
+++ test_case_criminal_data.py
@@ -31,35 +31,29 @@ def test_scrapper_valid_data():
     assert expected_output == actual_output
 
 
-# def test_scrapper_invalid_data():
-#     fname, lname, dob = 'Invalid', 'name', ''
-#     expected_output = {
-#         'docket_number': '',
-#         'court_office': '',
-#         'short_caption': '',
-#         'filling_date': '',
-#         'county': '',
-#         'primary_participant': '',
-#         'otn': '',
-#         'complaint_number': '',
-#         'dob': ''
-#     }
+def test_scrapper_invalid_data():
+    fname, lname, dob = 'Invalid', 'name', ''
+    expected_output = {
+        'docket_number': '',
+        'court_office': '',
+        'short_caption': '',
+        'filling_date': '',
+        'county': '',
+        'primary_participant': '',
+        'otn': '',
+        'complaint_number': '',
+        'dob': ''
+    }
 
-#     scrapper = CriminalDataScrapper(
-#         'https://ujsportal.pacourts.us/DocketSheets/MDJ.aspx', fname, lname, dob)
-#     resp1 = scrapper.make_request1()
-#     data = scrapper.parse_response1(resp1)
+    scrapper = CriminalDataScrapper(
+        'https://ujsportal.pacourts.us/DocketSheets/MDJ.aspx', fname, lname, dob)
+    resp1 = scrapper.make_request1()
+    data = scrapper.parse_response1(resp1)
 
-#     resp2 = scrapper.make_request2(data)
-#     data = scrapper.parse_response2(resp2)
+    resp2 = scrapper.make_request2(data)
+    data = scrapper.parse_response2(resp2)
 
-#     resp3 = scrapper.make_request3(data)
-#     actual_output = scrapper.parse_response3(resp3)
+    resp3 = scrapper.make_request3(data)
+    actual_output = scrapper.parse_response3(resp3)
 
-#     assert expected_output == actual_output
-
-
-# Taks To be done
-# 1. Run test cases
-# 2. Handle Invalid Scenario
-# 3. Handle Multiple Scenario
+    assert expected_output == actual_output
